b15ee733ca9d173d0e6ab179c23e3b11814c4ca8
juju4 <juju4@users.noreply.github.com>
2018-02-04 09:40:07 -0500

    add pf check

---
+++ controls/pf_spec.rb
@@ -0,0 +1,17 @@
+# encoding: utf-8
+
+control 'firewall-01' do
+  impact 1.0
+  title 'Ensure system firewall is configured'
+  desc 'Host firewall is part of defense in depth'
+  describe file('/private/etc/pf.conf') do
+    it { should be_file }
+    it { should be_owned_by 'root' }
+    its { mode cmp '0644' }
+  end
+  # requires root
+  describe command('pfctl -sr') do
+    its('stdout') { should match '/scrub-anchor/' }
+    its('stdout') { should match '/pass in log proto tcp from <private> to any port 22/' }
+  end
+end
