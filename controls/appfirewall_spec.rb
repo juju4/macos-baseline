647069019040e140e4ab5fe1cc36b91d024813a5
juju4 <juju4@users.noreply.github.com>
2018-02-04 09:40:19 -0500

    add appfirewall check

---
+++ controls/appfirewall_spec.rb
@@ -0,0 +1,22 @@
+# encoding: utf-8
+
+control 'firewall-01' do
+  impact 1.0
+  title 'Ensure application firewall is configured'
+  desc 'Host application firewall is part of defense in depth - root required'
+  describe command('/usr/libexec/ApplicationFirewall/socketfilterfw --getglobalstate') do
+    its('stdout') { should match '/Firewall is enabled. (State = 1)/' }
+  end
+  describe command('/usr/libexec/ApplicationFirewall/socketfilterfw --getloggingmode') do
+    its('stdout') { should match '/Log mode is on/' }
+  end
+  describe command('/usr/libexec/ApplicationFirewall/socketfilterfw --getallowsigned') do
+    its('stdout') { should match '/Automatically allow built-in signed software DISABLED/' }
+    its('stdout') { should match '/Automatically allow downloaded software DISABLED/' }
+  end
+  describe command('/usr/libexec/ApplicationFirewall/socketfilterfw --listapps') do
+    its('stdout') { should match '/NetAuthSysAgent/' }
+    its('stdout') { should match '/netbiosd/' }
+    its('stdout') { should match '/sshd-keygen-wrapper/' }
+  end
+end
