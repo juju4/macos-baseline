bcbeea3fe09453873a006f26acdb073793446e50
juju4 <juju4@users.noreply.github.com>
2018-02-04 09:39:28 -0500

    logging: review

--- controls/logging_spec.rb
+++ controls/logging_spec.rb
@@ -4,12 +4,18 @@ control 'logging-01' do
   impact 1.0
   title 'Ensure system logs are available'
   desc 'Logging is critical to system management'
-  describe file('/var/log/authd.log') do
+# not in vagrant macos?
+#  describe file('/var/log/authd.log') do
+#    it { should be_file }
+#    it { should be_owned_by 'root' }
+#    its('size') { should > 0 }
+#  end
+  describe file('/var/log/system.log') do
     it { should be_file }
     it { should be_owned_by 'root' }
     its('size') { should > 0 }
   end
-  describe file('/var/log/system.log') do
+  describe file('/var/log/install.log') do
     it { should be_file }
     it { should be_owned_by 'root' }
     its('size') { should > 0 }
